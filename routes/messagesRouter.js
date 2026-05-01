b1da430705032dc489212a25c899f7d8bd7552fb
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2026-05-01 11:50:07 -0400

    Delete no longer used file

--- routes/messagesRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const messagesRouter = Router();
-
-messagesRouter.get("/:id", (req, res) => {
-    res.render("message", { messages: messages });
-});
-
-module.exports = messagesRouter;
