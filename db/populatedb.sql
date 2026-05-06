7505bcac780ecd5349933580aaee40c28e6ca692
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2026-05-06 11:08:59 -0400

    Add script for populating the db
    The script should only be run once if there is no databse. It creates
    the database and populates it with 3 entries.

---
+++ db/populatedb.sql
@@ -0,0 +1,15 @@
+-- Connect to the db by runnign the PSQL command from Render, then run this script.
+
+-- Create new table
+CREATE TABLE IF NOT EXISTS messages (
+    if INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 32 ),
+    message VARCHAR ( 1024 )
+);
+
+-- Insert messages into the table
+INSERT INTO messages (name, message) 
+VALUES
+  ('Walter White', 'I am the one who knocks.'),
+  ('Jesse Pinkman', 'Yeah Mr White! Yeah, science!'),
+  ('Gustavo Fring', 'There will be... An appropriate response.');
